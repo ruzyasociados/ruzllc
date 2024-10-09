a44a2fa0db6993326b4dbd3c21db4285f713c490
Administrador <administrador@C4.local>
2024-10-09 16:11:17 -0600

    Diseño responsive

--- js/nav.js
+++ js/nav.js
@@ -13,3 +13,28 @@ window.addEventListener('scroll', function() {
     }
     lastScrollTop = scrollTop;
 });
+// Cerrar el menú de navegación al hacer clic en un enlace
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            const navbarCollapse = document.getElementById('navbarS');
+            if (navbarCollapse.classList.contains('show')) {
+                const bsCollapse = new bootstrap.Collapse(navbarCollapse);
+                bsCollapse.hide();
+            }
+        });
+    });
+});
+// Cerrar el menú de navegación al hacer clic fuera de él
+document.addEventListener('click', function(event) {
+    const navbarCollapse = document.getElementById('navbarS');
+    const toggler = document.querySelector('.navbar-toggler');
+
+    // Comprobar si el clic fue fuera del menú y del toggler
+    if (!navbarCollapse.contains(event.target) && !toggler.contains(event.target) && navbarCollapse.classList.contains('show')) {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse);
+        bsCollapse.hide();
+    }
+});
