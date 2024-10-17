7787fc8579426e2dce40d0a98a8cc68bb038c3c1
Administrador <administrador@C4.local>
2024-10-16 18:30:02 -0600

    Areglo de la barra de navegacion

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,21 @@
 let lastScrollTop = 0;
 const navbar = document.querySelector('.navbar');
-const scrollThreshold = 50; // Margen de error para el desplazamiento
 
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const windowHeight = window.innerHeight;
+    const halfWindowHeight = windowHeight / 2;
 
-    if (scrollTop > lastScrollTop + scrollThreshold) {
+    if (scrollTop > halfWindowHeight) {
         // Desplazamiento hacia abajo: Ocultar la barra de navegación
-        navbar.style.top = '-80px'; // Ajusta la altura de la barra de navegación según sea necesario
-    } else if (scrollTop < lastScrollTop - scrollThreshold) {
-        // Desplazamiento hacia arriba: Mostrar la barra de navegación
+        if (scrollTop > lastScrollTop) {
+            navbar.style.top = '-80px'; // Ajusta la altura de la barra de navegación según sea necesario
+        }
+    } else {
+        // Muestra la barra de navegación si el desplazamiento es menor a la mitad de la pantalla
         navbar.style.top = '0';
     }
+
     lastScrollTop = scrollTop;
 });
 
