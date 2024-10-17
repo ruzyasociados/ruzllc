1878fb7feba8c014546e14d6ecb4b4bc328b4bde
Administrador <administrador@C4.local>
2024-10-16 18:38:40 -0600

    Areglo de la barra de navegacionP3

--- js/nav.js
+++ js/nav.js
@@ -7,12 +7,15 @@ window.addEventListener('scroll', function() {
     const halfWindowHeight = windowHeight / 2;
 
     if (scrollTop > halfWindowHeight) {
-        // Desplazamiento hacia abajo: Ocultar la barra de navegación
+        // Solo oculta la barra si el usuario se desplaza hacia abajo
         if (scrollTop > lastScrollTop) {
             navbar.style.top = '-80px'; // Ajusta la altura de la barra de navegación según sea necesario
+        } else {
+            // Desplazamiento hacia arriba: Mostrar la barra de navegación
+            navbar.style.top = '0';
         }
     } else {
-        // Muestra la barra de navegación si el desplazamiento es menor a la mitad de la pantalla
+        // Si el usuario está en la parte superior, muestra la barra
         navbar.style.top = '0';
     }
 
